6df2de68a00535f44c26fccabd5dbf42f52fb8e2
Mayank Pandya <43351768+BackstageGit@users.noreply.github.com>
2023-02-11 14:56:10 +0530

    code refactor

--- companyapi/views.py
+++ companyapi/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse, JsonResponse
 
 
 def home_page(request):
-    print("Home Page Requested")
-    friends = ['AC','BB','CC']
+    #print("Home Page Requested")
+    #friends = ['AC','BB','CC']
     #return HttpResponse("This is Home Page")
     return JsonResponse(friends,safe=False)
     
